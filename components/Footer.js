2268a65717efd0c48df383964ba416b55389381f
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-04-30 10:30:01 -0500

    footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,28 @@
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from '../components/Footer.module.css';
 
 export default function Footer() {
   return (
     <>
-      <div className={`h-64 bg-black text-white text-center ${styles.footer}`}>
+      <div className={`h-auto bg-black text-white text-center ${styles.footer}`}>
         <div className='p-10'>
           <div className='my-4'>CONTACT US</div>
-          <div>
-
+          <div className='grid justify-items-center pb-5 text-4xl'>
+            <div>
+              <a href='https://www.instagram.com/gendotaiko/'>
+                <FontAwesomeIcon icon={faInstagram} className='mx-3'/>
+              </a>
+              <a href='https://www.facebook.com/gendotaiko/'>
+                <FontAwesomeIcon icon={faFacebook} className='mx-3'/>
+              </a>
+              <a href='https://www.youtube.com/channel/UCVY_EyNbypUi5v1kXF09GOQ'>
+                <FontAwesomeIcon icon={faYoutube} className='mx-3'/>
+              </a>
+            </div>
           </div>
           <div className={styles.info}>GENDO TAIKO</div>
           <div className={styles.info}>INFO@GENDOTAIKO.COM</div>
