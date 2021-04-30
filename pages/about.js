ebcccc15275703f29f5915b6a63e2af5efe88aea
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-04-30 11:18:34 -0500

    base styles

--- pages/about.js
+++ pages/about.js
@@ -17,13 +17,9 @@ export default function About() {
         <div className={`h-auto ${styles.container}`}>
           <div className='flex flex-wrap overflow-hidden lg:px-20 lg:pt-20'>
             <div className='w-full overflow-hidden p-12 lg:w-2/3 lg:px-12'>
-              <div className={`text-3xl lg:text-5xl ${styles.heading}`}>WHAT IS GENDO TAIKO?</div>
-              <div className={`leading-relaxed my-5 ${styles.bodytext}`}>
-                One of the earliest collegiate Taiko drumming groups established on the East Coast, Gendo Taiko was constituted at Brown University in the Fall of 2004 by Raiki Machida (Brown ’07) and Joshua Goldner (Brown ’05). Initially dubbed Brown Taiko, Gendo’s musical styles were originally based on those of Wadaiko Gensou, a semi-professional Taiko group based in Miyagi, Japan, with whom Machida had trained extensively. In the summer of 2006, Goldner and Machida hosted Wadaiko Gensou’s founding members Takao Handa and Satoko Nambu at Brown University to expand the group’s repertoire, hone its members’ techniques, and build a new set of drums. As the group grew in membership and developed its own unique musical styles and arrangements, it adopted a formal audition process, developed a Group Independent Study Project (GISP) to explore the history of Taiko, and is now in the course of developing a well-structured program of study for its members.
-              </div>
-              <div className={`leading-relaxed my-5 ${styles.bodytext}`}>
-                Today, Gendo Taiko continues to build and acquire new drums, expand its repertoire by composing new songs, and collaborate with the greater Taiko community by hosting and participating in annual conferences and invitationals. Handa-san and Nambu-san gave the team the name Gendo (幻道) during their stay in 2006. Gendo, loosely translating to “path of free imagination,” represents the journeys taken by members in years past, and the experience of creative inspiration we hope to pass down to future members and audiences in the years to come.
-              </div>
+              <h1 className={styles.heading}>WHAT IS GENDO TAIKO?</h1>
+              <p className={styles.bodytext}>One of the earliest collegiate Taiko drumming groups established on the East Coast, Gendo Taiko was constituted at Brown University in the Fall of 2004 by Raiki Machida (Brown ’07) and Joshua Goldner (Brown ’05). Initially dubbed Brown Taiko, Gendo’s musical styles were originally based on those of Wadaiko Gensou, a semi-professional Taiko group based in Miyagi, Japan, with whom Machida had trained extensively. In the summer of 2006, Goldner and Machida hosted Wadaiko Gensou’s founding members Takao Handa and Satoko Nambu at Brown University to expand the group’s repertoire, hone its members’ techniques, and build a new set of drums. As the group grew in membership and developed its own unique musical styles and arrangements, it adopted a formal audition process, developed a Group Independent Study Project (GISP) to explore the history of Taiko, and is now in the course of developing a well-structured program of study for its members.</p>
+              <p className={styles.bodytext}>Today, Gendo Taiko continues to build and acquire new drums, expand its repertoire by composing new songs, and collaborate with the greater Taiko community by hosting and participating in annual conferences and invitationals. Handa-san and Nambu-san gave the team the name Gendo (幻道) during their stay in 2006. Gendo, loosely translating to “path of free imagination,” represents the journeys taken by members in years past, and the experience of creative inspiration we hope to pass down to future members and audiences in the years to come.</p>
             </div>
             <div className={`w-full overflow-hidden p-12 lg:w-1/3 lg:py-20`}>
               <div className={`text-5xl mb-3 ${styles.character}`}>幻道太鼓</div>
@@ -34,16 +30,16 @@ export default function About() {
           </div>
           <div className='flex flex-wrap overflow-hidden lg:px-20 lg:pb-20'>
             <div className='w-full overflow-hidden p-12 lg:w-2/3 lg:px-12'>
-              <div className={`text-3xl lg:text-5xl ${styles.heading}`}>TAIKO IN AMERICA</div>
-              <div className={`leading-relaxed my-5 ${styles.bodytext}`}>
+              <h1 className={styles.heading}>TAIKO IN AMERICA</h1>
+              <p className={styles.bodytext}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Arcu dui vivamus arcu felis bibendum ut tristique et. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Purus non enim praesent elementum. Mauris in aliquam sem fringilla ut.
-              </div>
-              <div className={`leading-relaxed my-5 ${styles.bodytext}`}>
+              </p>
+              <p className={styles.bodytext}>
                 Eget arcu dictum varius duis at consectetur. Amet nisl suscipit adipiscing bibendum est ultricies. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Sit amet consectetur adipiscing elit ut aliquam purus sit. Magna ac placerat vestibulum lectus mauris ultrices eros in cursus. Ac tortor dignissim convallis aenean et. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Cras semper auctor neque vitae tempus quam. Arcu felis bibendum ut tristique et. Donec pretium vulputate sapien nec sagittis aliquam malesuada.
-              </div>
-              <div className={`leading-relaxed my-5 ${styles.bodytext}`}>
+              </p>
+              <p className={styles.bodytext}>
               Ullamcorper a lacus vestibulum sed arcu. Imperdiet proin fermentum leo vel orci porta. Bibendum arcu vitae elementum curabitur vitae nunc sed. Cursus turpis massa tincidunt dui ut ornare. Bibendum at varius vel pharetra.
-              </div>
+              </p>
             </div>
             <div className={`w-full overflow-hidden pb-12 lg:pt-24 lg:w-1/3`}>
               <Image
--- pages/index.js
+++ pages/index.js
@@ -30,16 +30,16 @@ export default function Home() {
             className='invisible md:visible'
           />
         </div>
-        <div className={`h-auto flex flex-wrap px-5 pb-12 lg:px-48 ${styles.info}`}>
+        <div className={`h-auto flex flex-wrap p-12 lg:px-48 ${styles.info}`}>
           <div className={`my-1 px-1 w-full overflow-hidden lg:my-6 lg:px-6 lg:w-2/3`}>
-            <div className={`text-3xl my-5 ${styles.subheading}`}>Mission statement sit amet, consectetur adipscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
-            <div className={`text-base ${styles.bodytext}`}>Ullamcorper a lacus vestibulum sed arcu. Imperdiet proin fermentum leo vel orci porta. Bibendum arcu vitae elementum curabitur vitae nunc sed. See more photos and videos of our performances here.</div>
+            <h2 className={styles.subheading}>Mission statement sit amet, consectetur adipscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h2>
+            <p className={styles.bodytext}>Ullamcorper a lacus vestibulum sed arcu. Imperdiet proin fermentum leo vel orci porta. Bibendum arcu vitae elementum curabitur vitae nunc sed. See more photos and videos of our performances here.</p>
           </div>
           <div className={`my-1 px-1 w-full overflow-hidden lg:my-6 lg:px-6 lg:w-1/3`}></div>
         </div>
         <hr />
         {/* <div className={`divide-y h-0.5`}></div> */}
-        <div className={` ${styles.info} h-auto flex flex-wrap overflow-hidden lg:px-12`}>
+        <div className={` ${styles.info} h-auto flex flex-wrap overflow-hidden p-6 lg:px-12`}>
           <div className='my-1 px-1 w-full overflow-hidden lg:my-6 lg:px-6 lg:w-1/2'>
             <div className='h-auto items-center m-5'>
               <div className="flex flex-wrap overflow-hidden">
@@ -100,8 +100,8 @@ export default function Home() {
               </div>
             </div>
             <div className='h-100 m-5 lg:m-12'>
-              <div className={`text-3xl m-5 ${styles.subheading}`}>Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam.</div>
-              <div className={`text-base m-5 ${styles.bodytext}`}>Ullamcorper a lacus vestibulum sed arcu. Imperdiet proin fermentum leo vel orci porta. Bibendum arcu vitae elementum curabitur vitae nunc sed. See more photos and videos of our performances here.</div>
+              <h2 className={styles.subheading}>Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam.</h2>
+              <p className={styles.bodytext}>Ullamcorper a lacus vestibulum sed arcu. Imperdiet proin fermentum leo vel orci porta. Bibendum arcu vitae elementum curabitur vitae nunc sed. See more photos and videos of our performances here.</p>
             </div>
             <div className='aspect-w-16 aspect-h-9 mx-5 lg:mx-12'>
               <iframe src='https://www.youtube.com/embed/uBDabxR74IU' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen></iframe>
@@ -109,8 +109,8 @@ export default function Home() {
           </div>
           <div className='my-1 w-full overflow-hidden lg:my-6 lg:px-6 lg:w-1/2'>
             <div className='h-auto m-5 lg:m-12'>
-              <div className={`text-3xl m-5 ${styles.subheading}`}>We perform eget arcu dictum varius duis at consectetur</div>
-              <div className={`text-base m-5 ${styles.bodytext}`}>Eget arcu dictum varius duis at consectetur. Amet nisl suscipit adipiscing bibendum est ultricies. Learn more about our upcoming and past events.</div>
+              <h2 className={styles.subheading}>We perform eget arcu dictum varius duis at consectetur</h2>
+              <p className={styles.bodytext}>Eget arcu dictum varius duis at consectetur. Amet nisl suscipit adipiscing bibendum est ultricies. Learn more about our upcoming and past events.</p>
             </div>
             <div className='h-auto m-5 lg:m-12'>
               <Image
@@ -122,8 +122,8 @@ export default function Home() {
               />
             </div>
             <div className='sm:h-108 lg:h-64 xl:h-64 m-5 lg:m-12'>
-              <div className={` text-3xl m-5 ${styles.subheading}`}>Gendo Taiko is made up of students who attend Brown University and the Rhode Island School of Design</div>
-              <div className={` text-base m-5 ${styles.bodytext}`}>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Learn more about our group’s history and  what taiko is all about here.</div>
+              <h2 className={styles.subheading}>Gendo Taiko is made up of students who attend Brown University and the Rhode Island School of Design</h2>
+              <p className={styles.bodytext}>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Learn more about our group’s history and  what taiko is all about here.</p>
             </div>
           </div>
         </div>
